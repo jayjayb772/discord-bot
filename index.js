2ef7d1b2fe93a703c0858fad5184e9e4305140cb
Jacob <jayjayb100@msn.com>
2020-04-08 13:28:38 -0500

    made my first bot script

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-import { botToken } from './sercrets';
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-bot.login(botToken)
+const { config } = require("dotenv");
+
+const bot = new Discord.Client({
+    disableEveryone: true
+    });
+
+config({
+    pah: __dirname + "/.env"
+});
+
+bot.on("ready", () =>{
+    console.log(`I am online, my name is ${bot.user.username}`);
+    bot.user.setPresence({
+        game:{
+            name: "being developed",
+            type: "WATCHING"
+        }
+    })
+
+});
+
+bot.on('message', async message =>{
+    const prefix = "_";
+    if (message.author.bot) return;
+    if(message.guild) return;
+    if(!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if(cmd === "ping"){
+        const msg = await message.channel.send('Pong');
+    }
+
+});
+
+
+bot.login(process.env.TOKEN);
