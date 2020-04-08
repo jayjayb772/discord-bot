670aa31259601637ec5d18ea233f0cc159f4b302
Jacob <jayjayb100@msn.com>
2020-04-08 12:18:41 -0500

    using secrets file for token

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hello World');
+import { botToken } from './sercrets';
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login(botToken)
---
+++ sercrets.js
@@ -0,0 +1,3 @@
+const botToken = 'Njk3NDk0NjE2NjE4MDQxNDQ2.Xo4Gmg.JCn-hQH4IKLoPMfu9MoJuysGCVY';
+
+export { botToken };
