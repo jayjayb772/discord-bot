0b7c5952596bac079837482ea15193602d2bfeac
Jacob <jayjayb100@msn.com>
2020-04-14 12:15:22 -0500

    I STG if it was a permissions issue imma be furious

--- automodFeatures.js
+++ automodFeatures.js
@@ -16,11 +16,9 @@ const checkMessage = async function (message, environment) {
     if (safe !== true) {
         const flagged = new MessageEmbed().setTitle(`Flagged message from ${message.author.tag} in ${message.channel.name}`).setDescription(message.content);
         const members = await message.guild.members.fetch();
-        //console.log(members);
 
         const managers = members.filter(m => m.roles.highest.id === process.env.Manager_ID);
 
-        console.log(managers);
         managers.forEach((manager)=>{
             //manager.send(flagged);
             console.log(manager.displayName);
