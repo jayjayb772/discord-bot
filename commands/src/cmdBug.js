6f6e4e7811fd9feaba4a36175645b2e5d6b6d2e0
Jacob <jayjayb100@msn.com>
2020-04-15 13:54:13 -0500

    [BUG FIX] added bot reaction to bug report

--- commands/src/cmdBug.js
+++ commands/src/cmdBug.js
@@ -3,13 +3,14 @@ const { MessageEmbed , Client} = require("discord.js");
 async function reportBug(message, timestamp){
 
     const bugReport = new MessageEmbed().setTitle(`Bug reported by ${message.author.tag}`)
-                                        .addField('Bug description', message.content
-                                                .toString().split('!bug')[1])
+                                        .addField('Bug description', message.content.toString().split('!bug')[1])
                                         .setFooter(timestamp);
 
     message.guild.members.fetch(process.env.bugReporter).then((user) =>{
         user.user.send(bugReport);
         message.author.send("Thank you for reporting a bug!");
+        const emoji = message.guild.emojis.cache.find(emoji =>emoji.name === 'NoAuthRequired');
+        message.react(emoji);
     })
 
 
