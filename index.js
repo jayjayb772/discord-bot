0a92c7d9d5ba7664047f8d8d04dced2f84c25932
Jacob <jayjayb100@msn.com>
2020-04-16 14:13:24 -0500

    add music commands, though non functional

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-const { MessageEmbed , Client} = require("discord.js");
-const { config } = require("dotenv");
+const {MessageEmbed, Client} = require("discord.js");
+const {config} = require("dotenv");
 const https = require('https');
 const {machines} = require("./commands/src/cmdMachines");
 //const {machines} = require("./commands/src/cmdMachines");
@@ -18,18 +17,18 @@ const {staff} = require("./commands/src/cmdStaff");
 
 const client = new Client({
     disableEveryone: true
-    });
+});
 
 config({
     pah: __dirname + "/.env"
 });
 
-client.on("ready",  async () =>{
+client.on("ready", async () => {
     console.log(`I am online, my name is ${client.user.username}`);
-    if(process.env.debug === "on") {
+    if (process.env.debug === "on") {
         await client.user.setActivity(`bugs run rampant`, {type: "WATCHING"});
-        client.guilds.cache.first().channels.cache.filter(channel => channel.id === process.env.onlineChannel).first().send(`Newly updated!`).then(r => r.delete({timeout:5000}));
-    }else{
+        client.guilds.cache.first().channels.cache.filter(channel => channel.id === process.env.onlineChannel).first().send(`Newly updated!`).then(r => r.delete({timeout: 5000}));
+    } else {
         await client.user.setActivity(`irl!help`, {type: "PLAYING"});
     }
 
@@ -37,24 +36,24 @@ client.on("ready",  async () =>{
 });
 
 
-
-client.on('message', async (message) =>{
+client.on('message', async (message) => {
     const prefix = "irl!";
     if (message.author.bot) return;
-    if(!message.guild) return;
+    if (!message.guild) return;
 
 
-
-    if(message.content.startsWith(prefix) && !message.author.bot){
+    if (message.content.startsWith(prefix) && !message.author.bot) {
         console.log(`${message.author.username} said ${message.content}`);
     }
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
-    if(!message.content.startsWith(prefix)){
+    if (!message.content.startsWith(prefix)) {
         await checkMessage(message, convertTimestamp(message.createdTimestamp));
         return;
     }
+
+
     switch (cmd) {
         case "hours":
             await hours(message, args);
@@ -69,7 +68,7 @@ client.on('message', async (message) =>{
             break;
 
         case "help":
-            await help(message,args);
+            await help(message, args);
             break;
 
         case "site":
@@ -85,7 +84,7 @@ client.on('message', async (message) =>{
             break;
 
         case "staff":
-            await staff(message,args, process.env.environment);
+            await staff(message, args, process.env.environment);
             break;
 
         case "upcoming":
@@ -98,21 +97,56 @@ client.on('message', async (message) =>{
         case "NO_CMD":
             break;
 
+        case "play":
+            if (message.channel.id === process.env.musicListener) {
+                await notFunctional(message)
+            } else {
+                await message.channel.send("Cannot use previous command in this channel").then(r => r.delete({timeout: 5000}))
+            }
+            message.delete();
+            break;
+
+        case "skip":
+            if (message.channel.id === process.env.musicListener) {
+                await notFunctional(message)
+            } else {
+                await message.channel.send("Cannot use previous command in this channel").then(r => r.delete({timeout: 5000}))
+            }
+            message.delete();
+            break;
+
+        case "pause":
+            if (message.channel.id === process.env.musicListener) {
+                await notFunctional(message)
+            } else {
+                await message.channel.send("Cannot use previous command in this channel").then(r => r.delete({timeout: 5000}))
+            }
+            message.delete();
+            break;
+
+        case "stop":
+            if (message.channel.id === process.env.musicListener) {
+                await notFunctional(message)
+            } else {
+                await message.channel.send("Cannot use previous command in this channel").then(r => r.delete({timeout: 5000}))
+            }
+            message.delete();
+            break;
         default:
             await notCMD(message);
     }
 
 });
 
-function convertTimestamp(timestamp, offset = -6){
+function convertTimestamp(timestamp, offset = -6) {
     let d = new Date(timestamp);
     let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-    let nd = new Date(utc + (3600000*offset));
+    let nd = new Date(utc + (3600000 * offset));
     return nd.toLocaleString();
 }
 
 
-client.login(process.env.TOKEN).catch((error) =>{
+client.login(process.env.TOKEN).catch((error) => {
     console.log(error);
 });
 
