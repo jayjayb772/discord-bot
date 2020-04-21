fce4cbc8c7410b378777d089b054e65788665b2e
Jacob <jayjayb100@msn.com>
2020-04-21 16:55:28 -0500

    Adds welcome message for new members.

--- commands/src/welcomeAcknowledge.js
+++ commands/src/welcomeAcknowledge.js
@@ -5,7 +5,10 @@ async function welcome(member){
     const guild = member.guild;
     const introChannel = guild.channels.cache.filter(channel => channel.id === process.env.introChannel).first();
     introChannel.send(`Welcome to the IRL community Discord server, ${member.user.tag}!`);
-    await member.user.send("Welcome to the IRL discord server. Be sure to check out the #guidelines channel for our rules, and introduce yourself in the #introductions channel!")
+    await member.user.send("Welcome to the IRL discord server. Be sure to check out the #guidelines channel for our rules, and introduce yourself in the #introductions channel!").catch((err)=>{
+        console.log("Error sending welcome message to user:");
+        console.log(err);
+    })
 }
 
 module.exports = {welcome};
